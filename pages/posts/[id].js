c3dc7618249785fdd0862c1cd18111ff01b45aeb
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2022-01-19 18:32:30 +0700

    feat: 🎊 ready for deploy

---
+++ pages/posts/[id].js
@@ -0,0 +1,37 @@
+import Layout from "../../components/layout";
+import { getAllPostsIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyle from "../../styles/utils.module.css";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <h1 className={utilStyle.headingXl}>{postData.title}</h1>
+      <div className={utilStyle.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllPostsIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
